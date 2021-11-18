42b56dd8d3d294c9dd732fce43626b757fe3b75d
chenlipeng <55403225@qq.com>
2021-11-18 21:58:39 +0800

    add login

--- easy-im/src/App.tsx
+++ easy-im/src/App.tsx
@@ -1,43 +1,30 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { Row, Col, Image } from 'antd';
 import './App.css'
+import 'antd/dist/antd.css'
+import EasyIm from './img/easy-im.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='easy-container'>
+        <div style={{position:'absolute',left:'150px',top:'100px'}}>
+        <Image
+          width={125}
+          src={EasyIm}
+          style={{borderRadius:'25px'}}
+        />
+        <span className='fontStyle'>易聊</span>
+      </div>
+      {/* <Row>
+        <Col span={18} style={{ background: 'blue',float:'left' }}>
+          col-6 col-pull-18
+        </Col>
+        <Col span={6} style={{ background: 'red' }}>
+        </Col>
+
+      </Row> */}
     </div>
   )
 }
